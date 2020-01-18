92ae55e4c9825f80ff8e05d0cbb6f2169440fb46
Anna Arutyunyan <arutik@gmail.com>
2020-01-18 21:33:20 +0000

    Update get_pt24h_data.py

--- get_pt24h_data.py
+++ get_pt24h_data.py
@@ -11,6 +11,19 @@ app = Flask(__name__)
 def hello():
   return "Hello"
 
+# if this is True, user sets a threshold on CO2 emission level manually
+# if False, the times of charging
+manual = False
+
+# threshold of environmentally friendly CO2 emission levels to use as a limit for charging devices
+threshold = 100
+
+# what time the device needs to be fully charged
+time = 2
+
+# how much time is available for changing the device
+window = 5
+
 
 @app.route('/get_graph_data', methods=['GET'])
 #@cross_origin(supports_credentials=True)
@@ -36,5 +49,41 @@ def get_data():
     dict_object = {'data': emission_intensities}
 
     return str(dict_object)
+
+@app.route('/set_params', methods=['POST'])
+
+def set_params():
+
+        manual = request.form.get('manual')
+        try:
+                bool_manual = bool(manual)
+        except ValueError:
+                return('Input manual is not boolean.')
+
+        threshold = request.form.get('threshold')
+
+        try:
+                int_threshold = int(threshold)
+        except ValueError:
+                return('Input threshold is not integer.')
+
+        time = request.form.get('time')
+        try:
+                int_time = int(time)
+        except ValueError:
+                return('Input time is not integer.')
+
+        window = request.form.get('window')
+        try:
+                int_window = int(window)
+        except ValueError:
+                return('Input window is not integer.')
+
+
+@app.route('/get_charging_state', methods=['GET'])
+
+def get_charging_status():
+        return({'state': 0})
+
 if __name__ == '__main__':
     app.run()
